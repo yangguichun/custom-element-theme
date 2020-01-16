2e1ead559381093558556cbeb1028ac6bec6876b
yangguichun@foxmail.com <yangguichun@foxmail.com>
2020-01-16 15:10:36 +0800

    重构gulp，将整个业务流都放在gulp中，基本可用，还需要调整结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var path = require('path')
+var fs = require('fs')
 var gulp = require('gulp')
-const {series, parallel } = require('gulp')
+var rename = require('gulp-rename')
+
+const {series, task, parallel } = require('gulp')
+
+const {build, fonts} = require('./lib/task')
+const {update_theme_variables} = require('./lib/update-element-theme-variable')
+
 var cleanCSS = require('gulp-clean-css');
 var cssWrap = require('gulp-css-wrap');
 let minimist = require('minimist');
 
 let defaultOption = {
   string:'n',
-  default: { n: 'custome-theme'}
+  default: { n: 'custom-theme'}
 }
 let options  = minimist(process.argv.slice(2), defaultOption)
 let customThemeName = options.n
@@ -27,4 +34,20 @@ function copyConfig() {
   return gulp.src('./config.json').pipe(gulp.dest(`dist/${customThemeName}`));
 }
 
-exports.default = parallel(wrapCss, moveFont, copyConfig)
+function del_vars_file(done){
+  let filePath = './element-variables.scss'
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath)
+  }
+  done()
+}
+
+function init_vars() {
+  var config = require('./lib/config')
+  var varsPath = path.resolve(config.themePath, './src/common/var.scss')  
+  return gulp.src(varsPath)
+    .pipe(rename('element-variables.scss'))
+    .pipe(gulp.dest('./'))
+}
+
+exports.default = series(del_vars_file, init_vars, update_theme_variables, build, fonts, parallel(wrapCss, moveFont, copyConfig))
